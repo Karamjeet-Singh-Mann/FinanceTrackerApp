3fe20ec7e13a63f4f28937cf0bd5ed7e8d0e0c08
karamjeet singh <karamjeet.singh.mann.work@gmail.com>
2026-04-30 06:12:53 +0530

    Initial Commit

---
+++ Finance Tracker App/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Finance Tracker App
+//
+//  Created by Karamjeet singh on 30/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Finance Tracker AppTests/Finance_Tracker_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  Finance_Tracker_AppTests.swift
+//  Finance Tracker AppTests
+//
+//  Created by Karamjeet singh on 30/04/26.
+//
+
+import Testing
+@testable import Finance_Tracker_App
+
+struct Finance_Tracker_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
